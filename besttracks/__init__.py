477f648cae932ec8ff04356d7d46a4ce85d62c4c
Qianye Su <112712604+SQYQianYe@users.noreply.github.com>
2025-03-25 17:32:16 +0800

    Add files via upload

--- besttracks/__init__.py
+++ besttracks/__init__.py
@@ -5,4 +5,4 @@ from .core import Particle, TC, Drifter, ParticleSet, TCSet, DrifterSet
 from .utils import plot_tracks, plot_track, plot_intensity, plot_intensities, \
                    plot, binning, binning_particle, binning_particles
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
